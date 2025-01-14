8a40ebd31212605eae30be921d49647cd77ad363
Shrafi Shehab <svshuvo696@gmail.com>
2025-01-14 12:44:42 +0600

    Basic header structure is complete

--- src/pages/shared/Header.jsx
+++ src/pages/shared/Header.jsx
@@ -1,8 +1,79 @@
+import { HiBars3BottomLeft } from "react-icons/hi2";
+import { IoMdNotifications } from "react-icons/io";
+import { MdOutlineNotifications } from "react-icons/md";
 
 const Header = () => {
+    const menuItems =
+                    <>
+                        <li><a>Home</a></li>
+                        <li><a>Membership</a></li>
+                    </>
     return (
-        <div>
-            header
+        <div className="navbar bg-secondaryColor">
+            <div className="navbar-start">
+                <div className="dropdown">
+                    <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                        <HiBars3BottomLeft size={25} color="#fff10f"/>
+                    </div>
+                    <ul
+                        tabIndex={0}
+                        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
+                        {menuItems}
+                    </ul>
+                </div>
+                <a className="btn btn-ghost text-xl">GuildDo</a>
+            </div>
+            <div className="navbar-center hidden lg:flex">
+                <ul className="menu menu-horizontal px-1">
+                    {menuItems}
+                </ul>
+            </div>
+            <div className="navbar-end">
+                <div className="flex-none">
+                    <div className="dropdown dropdown-end">
+                        <div tabIndex={0} role="button" className="btn btn-ghost btn-circle">
+                            <div className="indicator">
+                                <MdOutlineNotifications size={23} color="#fff10f" />
+                                <span className="badge badge-sm indicator-item">8</span>
+                            </div>
+                        </div>
+                        <div
+                            tabIndex={0}
+                            className="card card-compact dropdown-content bg-base-100 z-[1] mt-3 w-52 shadow">
+                            <div className="card-body">
+                                <span className="text-lg font-bold">8 Items</span>
+                                <span className="text-info">Subtotal: $999</span>
+                                <div className="card-actions">
+                                    <button className="btn btn-primary btn-block">View cart</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="dropdown dropdown-end">
+                        <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
+                            <div className="w-10 rounded-full">
+                                <img
+                                    alt="Tailwind CSS Navbar component"
+                                    src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+                            </div>
+                        </div>
+                        <ul
+                            tabIndex={0}
+                            className="menu menu-sm dropdown-content bg-secondaryColor rounded-box z-[1] mt-3 w-52 p-2 shadow">
+                            <li>
+                                <a className="justify-between">
+                                    Profile
+                                    <span className="badge">New</span>
+                                </a>
+                            </li>
+                            <li><a>Settings</a></li>
+                            <li><a>Logout</a></li>
+                        </ul>
+                    </div>
+                </div>
+                
+                <a className="btn bg-primaryColor">Join US</a>
+            </div>
         </div>
     );
 };
